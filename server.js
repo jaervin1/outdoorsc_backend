416b7b1b6b10899728af34ffc307a0e7a0a3f60e
jaervin1 <jaervin@email.sc.edu>
2025-04-06 15:26:51 -0400

    fixed maybe

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 let activities = [{
